4e02c6146a9dadca544e9b80b2609101a172c581
neko0xff <neko_0xff@protonmail.com>
2024-08-17 05:34:56 +0800

    20240816 update2

--- astro.config.ts
+++ astro.config.ts
@@ -1,21 +1,26 @@
 import { defineConfig } from "astro/config";
+import { SITE } from "./src/config";
+import { mermaid } from "./src/plugins/mermaid.ts";
+import { proseRemarkPlugin } from './src/plugins/prose-remark-plugin.mjs';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
-import { SITE } from "./src/config";
 import remarkMath from 'remark-math';
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
-import remarkGraphviz from 'remark-graphviz';
-//import remarkMermaid from 'astro-diagram/remark-mermaid';
+import remarkGraphviz from "remark-graphviz";
+import remarkMermaid from "remark-mermaid";
 import rehypeKatex from 'rehype-katex';
 import rehypeStringify from "rehype-stringify";
 import rehypeMermaid from "rehype-mermaid";
 import rehypeGraphviz from "rehype-graphviz";
-//import redotStringify from "redot-stringify";
+import redotStringify from "redot-stringify";
 import rehypeRaw from 'rehype-raw';
+
+import vue from "@astrojs/vue";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
@@ -26,26 +31,28 @@ export default defineConfig({
     }),
     react(),
     sitemap(),
+    vue(),
+    mdx()
   ],
   markdown: {
-   // redotPlugins: [
-      //redotStringify,
-   // ],
+    redotPlugins: [
+      redotStringify,
+    ],
     rehypePlugins: [
       rehypeRaw,
       rehypeKatex,
-      //rehypeMermaid,
+      rehypeMermaid,
       rehypeStringify,
       rehypeGraphviz
     ],
     remarkPlugins: [
-      //proseRemarkPlugin,
+      proseRemarkPlugin,
       remarkMath,
       remarkParse,
       remarkRehype,
       remarkGraphviz,
-      //remarkMermaid,
-      //mermaid,
+      remarkMermaid,
+      mermaid,
       remarkToc,
       [
         remarkCollapse,
@@ -56,7 +63,10 @@ export default defineConfig({
     ],
     shikiConfig: {
       // For more themes, visit https://shiki.style/themes
-      themes: { light: "min-light", dark: "night-owl" },
+      themes: { 
+        light: "material-theme-lighter",
+        dark: "material-theme-darker" 
+      },
       wrap: true,
     },
   },
---
+++ redot-stringify.d.ts
@@ -0,0 +1,1 @@
+declare module "redot-stringify";
