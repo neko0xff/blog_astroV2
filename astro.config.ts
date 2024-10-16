005ee8de0ad2b523239a35481a254a093b119f2c
neko0xff <chzang55@gmail.com>
2024-10-16 09:55:37 +0800

    20241016 update1

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, passthroughImageService } from "astro/config";
 import { SITE } from "./src/config";
 import { mermaid } from "./src/plugins/mermaid.ts";
+import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs';
 import { proseRemarkPlugin } from "./src/plugins/prose-remark-plugin.mjs";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
@@ -23,12 +24,11 @@ import wasm from 'vite-plugin-wasm';
 import markdoc from "@astrojs/markdoc";
 // add deno support
 import deno from '@deno/astro-adapter';
-//import nodeLoaderPlugin from "@vavite/node-loader/plugin";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
-  output: 'hybrid', // 開啟 prerender 預先渲染: 
+  output: 'hybrid', // 開啟 prerender 預先渲染
   adapter: deno(),
   image: {
     service: passthroughImageService(),
@@ -41,7 +41,7 @@ export default defineConfig({
     sitemap(),
     mdx(),
     lighthouse(),
-    markdoc()
+    markdoc(),
   ],
   markdown: {
     rehypePlugins: [
@@ -60,6 +60,7 @@ export default defineConfig({
       remarkMermaid,
       mermaid,
       remarkToc,
+      remarkReadingTime,
       [
         remarkCollapse,
         {
---
+++ src/components/react/Comment.tsx
@@ -0,0 +1,83 @@
+import * as React from 'react';
+import Giscus from '@giscus/react';
+
+const id = 'inject-comments';
+
+// 獲得模式切換的狀態
+function getSavedTheme() {
+  return window.localStorage.getItem('theme');
+}
+
+// 決定是否切換夜間模式
+function getSystemTheme() {
+  return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+}
+
+const Comments = () => {
+  const [mounted, setMounted] = React.useState(false);
+  const [theme, setTheme] = React.useState('light');
+
+  React.useEffect(() => {
+    const theme = getSavedTheme() || getSystemTheme()
+    setTheme(theme)
+    // 監聽使用者是否切換夜間模式
+    const observer = new MutationObserver(() => {
+      setTheme(getSavedTheme());
+    });
+
+    observer.observe(document.documentElement, {
+      attributes: true,
+      attributeFilter: ['data-theme'],
+    });
+
+    return () => {
+      observer.disconnect()
+    }
+  }, [])
+
+  React.useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  return (
+    <div id={id} className="w-full">
+      {mounted ? (
+        <Giscus
+          id={id}
+          repo="neko0xff/blog_astroV2"
+          repoId="R_kgDOMkI6YA"
+          category="Announcements"
+          categoryId="DIC_kwDOMkI6YM4CjZSo"
+          mapping="title"
+          reactionsEnabled="1"
+          emitMetadata="0"
+          inputPosition="top"
+          lang="zh-TW"
+          loading="lazy"
+          theme={theme}
+        />
+      ) : null}
+    </div>
+  )
+}
+
+/* 透過 giscus.app 輸出
+<script src="https://giscus.app/client.js"
+        data-repo="neko0xff/blog_astroV2"
+        data-repo-id="R_kgDOMkI6YA"
+        data-category="Announcements"
+        data-category-id="DIC_kwDOMkI6YM4CjZSo"
+        data-mapping="title"
+        data-strict="0"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="top"
+        data-theme="dark_dimmed"
+        data-lang="zh-TW"
+        data-loading="lazy"
+        crossorigin="anonymous"
+        async>
+</script>
+*/
+
+export default Comments
