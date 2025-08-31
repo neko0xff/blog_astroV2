b74e6e3cac40272ba7668f4895ce3fb8f102ba52
neko_0xff <54382007+neko0xff@users.noreply.github.com>
2025-08-31 11:06:30 +0800

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -55,6 +55,7 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
     optimizeDeps: {
+      include: ["react-dom", "react"],
       exclude: ["@resvg/resvg-js"],
     },
     resolve: {
