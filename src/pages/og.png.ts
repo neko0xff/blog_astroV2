823622bc300e4c3f7cf90bb5b030705b2176b83c
neko_0xff <54382007+neko0xff@users.noreply.github.com>
2026-02-20 08:40:59 +0800

    來自雲端代理程式工作階段的 VS Code 的檢查點

--- src/pages/og.png.ts
+++ src/pages/og.png.ts
@@ -1,7 +1,19 @@
 import type { APIRoute } from "astro";
 import { generateOgImageForSite } from "@/utils/generateOgImages.ts";
 
-export const GET: APIRoute = async () =>
-  new Response(await generateOgImageForSite(), {
+export const GET: APIRoute = async () => {
+  const buffer = await generateOgImageForSite();
+  let uint8Array: Uint8Array;
+  if (buffer instanceof Uint8Array) {
+    uint8Array = buffer;
+  } else if (buffer instanceof ArrayBuffer) {
+    uint8Array = new Uint8Array(buffer);
+  } else if (typeof Buffer !== "undefined" && buffer instanceof Buffer) {
+    uint8Array = new Uint8Array(buffer.buffer, buffer.byteOffset, buffer.byteLength);
+  } else {
+    throw new Error("Unsupported buffer type returned from generateOgImageForSite");
+  }
+  return new Response(uint8Array, {
     headers: { "Content-Type": "image/png" },
   });
+};
